cd04cfc67bab2e3610341777138f2a8b2dd1b490
Sohrab momand <sohrabmomand.work@gmail.com>
2023-11-09 07:31:26 +0430

    still trying to bulid the components

--- src/App.js
+++ src/App.js
@@ -4,31 +4,29 @@ const messages = [
   "Invest your new income 🤑",
 ];
 
+
+
+
 export default function App() {
 
-  const step = 2;
+  const step = 1;
 
 
-  function handleprevouis() {
-    alert("Please");
-  }
 
 
 
   return <div className="steps">
     <div className="numbers">
       <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-      <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-      <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      <div className={`${step >= 1 ? "active" : ""}`}>2</div>
+      <div className={`${step >= 1 ? "active" : ""}`}>3</div>
     </div>
 
-    <p className="message">
-      Step {step}: {messages[step - 1]}
-    </p>
+    <p className="message">Step {step}: {messages[step - 1]}</p>
 
     <div className="buttons">
-      <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handleprevouis}>prevouis</button>
-      <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>next</button>
+      <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>previous</button>
+      <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>next</button>
     </div>
   </div>
 }
